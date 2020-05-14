e46934c86434448ee6beb74de8e90a0a5da1b1fb
Ram Krishna <krishram101@gmail.com>
2020-05-15 00:13:45 +0530

    Solution for trie

--- trie-implementation.js
+++ trie-implementation.js
@@ -16,13 +16,13 @@
 /**
  * Initialize your data structure here.
  */
-var TrieNode = function () {
-  this.val = val ? val : null;
-  this.child = new Map();
+var TrieNode = function (val = null) {
+  this.val = val;
+  this.children = new Map();
+  this.end = false;
 };
-
 var Trie = function () {
-  this.root = TrieNode();
+  this.root = new TrieNode();
 };
 
 /**
@@ -30,21 +30,46 @@ var Trie = function () {
  * @param {string} word
  * @return {void}
  */
-Trie.prototype.insert = function (word) {};
+Trie.prototype.insert = function (word) {
+  let parent = this.root;
+
+  for (let i = 0; i < word.length; i++) {
+    if (!parent.children.has(word[i]))
+      parent.children.set(word[i], new TrieNode(word[i]));
+    parent = parent.children.get(word[i]);
+    if (i === word.length - 1) parent.end = true;
+  }
+};
 
 /**
  * Returns if the word is in the trie.
  * @param {string} word
  * @return {boolean}
  */
-Trie.prototype.search = function (word) {};
+Trie.prototype.search = function (word) {
+  let parent = this.root;
+
+  for (let i = 0; i < word.length; i++) {
+    if (!parent.children.has(word[i])) return false;
+    parent = parent.children.get(word[i]);
+    if (i === word.length - 1) return parent.end;
+  }
+};
 
 /**
  * Returns if there is any word in the trie that starts with the given prefix.
  * @param {string} prefix
  * @return {boolean}
  */
-Trie.prototype.startsWith = function (prefix) {};
+Trie.prototype.startsWith = function (prefix) {
+  let parent = this.root;
+
+  for (let i = 0; i < prefix.length; i++) {
+    if (!parent.children.has(prefix[i])) return false;
+    parent = parent.children.get(prefix[i]);
+    if (i === prefix.length - 1) return true;
+  }
+};
 
 /**
  * Your Trie object will be instantiated and called as such:
