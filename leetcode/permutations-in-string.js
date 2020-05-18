06a48d0b9aa504b5ce809885bfb5b501aa9c5829
Ram Krishna <krishram101@gmail.com>
2020-05-19 00:04:20 +0530

    Updated equals function in JS

--- leetcode/permutations-in-string.js
+++ leetcode/permutations-in-string.js
@@ -16,47 +16,54 @@
     The length of both given strings is in range [1, 10,000].
 */
 
-// /**
-//  * @param {string} s1
-//  * @param {string} s2
-//  * @return {boolean}
-//  */
-// var checkInclusion = function(s1, s2) {
-//     let char_count = [];
-//     let s1Hash = [], s2Hash = [];
-//     if(s1.length > s2.length)
-//         return false;
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+  if (s1.length > s2.length) return false;
 
-//     let left = 0, right = 0;
+  let s1Hash = {},
+    s2Hash = {};
+  if (s1.length > s2.length) return false;
 
-//     while (right < s1.length) {
-//         let rightInd = s1.charCodeAt(right) - 97;
-//         s1Hash[rightInd] = s1Hash[rightInd] || 0;
-//         s1Hash[rightInd]++;
+  let left = 0,
+    right = 0;
 
-//         let rightInd1 = s2.charCodeAt(right) - 97;
-//         s2Hash[rightInd1] = s2Hash[rightInd1] || 0;
-//         s2Hash[rightInd1]++;
-//         right++;
-//     }
-//     console.log(right, s1Hash, s2Hash)
-//     right--;
-//     while (right < s2.length) {
-// //this hash equals doesn't work in Javascript.
-//         if(s1Hash === s2Hash)
-//             return true;
+  while (right < s1.length) {
+    let rightInd = s1.charAt(right);
+    s1Hash[rightInd] = s1Hash[rightInd] || 0;
+    s1Hash[rightInd]++;
 
-//         right++;
+    let rightInd1 = s2.charAt(right);
+    s2Hash[rightInd1] = s2Hash[rightInd1] || 0;
+    s2Hash[rightInd1]++;
+    right++;
+  }
+  right--;
 
-//         if (right != s2.length) {
-//             let rightInd = s2.charCodeAt(right) - 97;
-//             s2Hash[rightInd] = s2Hash[rightInd] || 0;
-//             s2Hash[rightInd]++;
-//         }
-//         let leftInd = s2.charCodeAt(left) - 97;
-//         s2Hash[leftInd] = s2Hash[leftInd] || 0;
-//         s2Hash[leftInd]--;
-//         left++;
-//     }
-//     return false;
-// };
+  while (right < s2.length) {
+    if (checkEquals(s1Hash, s2Hash)) return true;
+
+    right++;
+
+    if (right != s2.length) {
+      let rightInd = s2.charAt(right);
+      s2Hash[rightInd] = s2Hash[rightInd] || 0;
+      s2Hash[rightInd]++;
+    }
+    let leftInd = s2.charAt(left);
+    s2Hash[leftInd] = s2Hash[leftInd] || 0;
+    s2Hash[leftInd]--;
+    left++;
+  }
+  return false;
+};
+
+var checkEquals = function (str1, str2) {
+  for (const i in str1) {
+    if (str1[i] !== str2[i]) return false;
+  }
+  return true;
+};
