b8f6bbec2b7e70d88fd6b4ad688c8b481d40bf4f
Ram Krishna <krishram101@gmail.com>
2020-07-24 21:24:38 +0530

    solution for single number 3

---
+++ leetcode/single-number-3.js
@@ -0,0 +1,27 @@
+/**
+ * Single Number III
+
+    Solution
+    Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. 
+    Find the two elements that appear only once.
+
+    Example:
+    Input:  [1,2,1,3,2,5]
+    Output: [3,5]
+
+    Note:
+    The order of the result is not important. So in the above example, [5, 3] is also correct.
+    Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function (nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) x ^= nums[i];
+  x &= -x;
+  let result = [0, 0];
+  for (let n of nums) result[(x & n) == 0 ? 0 : 1] ^= n;
+  return result;
+};
